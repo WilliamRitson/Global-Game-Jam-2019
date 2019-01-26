c5009073bcc6b0717f1e48aebf1b3ca27c5971ad
William Ritson <william.ritson@gmail.com>
2019-01-25 21:42:13 -0800

    Added level 2 with simple test script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 moveDirection;
+    public float moveSpeed;
+    public float manuverSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += (Vector3) moveDirection.normalized * Time.deltaTime  * moveSpeed;
+        this.transform.position += (Vector3) new Vector2(Input.GetAxis("Horizontal"), 0).normalized * Time.deltaTime * manuverSpeed;
+    }
+}
