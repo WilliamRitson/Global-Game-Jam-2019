203dd3d1a2c0058cecb9ce09e1f9add6cf20c9a3
jthpotz <jthpotz@gmail.com>
2019-01-26 11:27:19 -0800

    started on falling obstacles

---
+++ Assets/Scripts/Controller/TriggerFall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TriggerFall : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    Ray ray;
+    RaycastHit2D[] hits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ray = new Ray(gameObject.transform.position, Vector3.down);
+        hits = Physics2D.GetRayIntersectionAll(ray);
+        if (hits.Select (n => CheckHitPlayer(n)).Contains(true))
+        {
+            rb.gravityScale = 0.75f;
+        }
+    }
+
+    private bool CheckHitPlayer(RaycastHit2D hit)
+    {
+        if (hit.collider.gameObject.layer == 9)
+        {
+            return true;
+        }
+        return false;
+    }
+}
