ec2886c3534f0bb7b56f64eed4ceb6d94da2004c
jthpotz <jthpotz@gmail.com>
2019-01-26 11:51:56 -0800

    more work on falling obj

--- Assets/Scripts/Controller/TriggerFall.cs
+++ Assets/Scripts/Controller/TriggerFall.cs
@@ -14,17 +14,16 @@ public class TriggerFall : MonoBehaviour
     void Start()
     {
         gameObject.GetComponent<Rigidbody2D>();
+        if (playerLayer == null)
+        {
+            playerLayer = GameObject.FindGameObjectWithTag("Player").layer;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector2 position = transform.position;
-        Vector2 direction = Vector2.down;
-        float distance = 1.0f;
-
-        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, playerLayer);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 100, playerLayer);
 
         if (hit.collider != null)
         {
