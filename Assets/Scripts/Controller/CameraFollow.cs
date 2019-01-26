3c5aa07231d4e044b4182344c8006cfa2b005305
jthpotz <jthpotz@gmail.com>
2019-01-26 13:12:28 -0800

    camera follow

---
+++ Assets/Scripts/Controller/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
