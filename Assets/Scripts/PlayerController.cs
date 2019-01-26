52f98ba63a6fea48666caa5fd8f78a74c2c25e2e
jthpotz <jthpotz@gmail.com>
2019-01-26 13:59:29 -0800

    script for obstacles that shove you

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -87,6 +87,10 @@ public class PlayerController : MonoBehaviour
             collision.gameObject.GetComponent<BreakFall>().Fall();
             this.takeDamage();
         }
+        if (collision.gameObject.CompareTag("ShoveObject"))
+        {
+            collision.gameObject.GetComponent<ShoveDirection>().Shove(gameObject);
+        }
     }
 
     void takeDamage()
